d4e10246696269c57997d1dfe78b02f74879424d
Paweł Kumanowski <pkumanowski@outlook.com>
2018-12-03 22:44:48 +0100

    second commit

--- src/Okno.java
+++ src/Okno.java
@@ -6,28 +6,26 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JList;
 
-import com.owca.Stado;
-
 public class Okno extends JFrame {
 	Kontrahent kontrahent;
 	private JList<Faktura> lista;
 	
 	
-	public Okno(int w, int h, String title,Stado s) {
-		stado = s;
+	public Okno(int w, int h, String title, Kontrahent k) {
+		kontrahent = k;
 		setSize(w, h);
 		setTitle(title);
 		
-		JComponent po = przygotujPanelKontrahenta();
-		po.setPreferredSize(new Dimension(w/2, h));
-		po.setBackground(Color.YELLOW);
-		JComponent ps = przygotujPanelFaktur(w/2,h);
+		JComponent pk = przygotujPanelKontrahenta();
+		pk.setPreferredSize(new Dimension(w/2, h));
+		pk.setBackground(Color.YELLOW);
+		JComponent pf = przygotujPanelFaktur(w/2,h);
 		//ps.setPreferredSize(new Dimension(w/2, h));
-		ps.setBackground(Color.RED);
+		pf.setBackground(Color.RED);
 		
 		getContentPane().setLayout(new FlowLayout());
-		getContentPane().add(po);
-		getContentPane().add(ps);
+		getContentPane().add(pk);
+		getContentPane().add(pf);
 		
 		pack();
 		setVisible(true);
